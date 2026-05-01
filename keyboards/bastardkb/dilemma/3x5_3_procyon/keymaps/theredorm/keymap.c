ea2f1f07f96e1461980f395f5c011753e7d9fc13
Andrew <theredorm@gmail.com>
2026-05-01 12:43:28 +0200

    temp

--- keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/theredorm/keymap.c
+++ keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/theredorm/keymap.c
@@ -85,7 +85,7 @@ const key_override_t *key_overrides[] = {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [LAYER_BASE] = LAYOUT_split_3x5_3(
     KC_Q,            KC_W,            KC_E,            KC_R,            KC_T,              KC_Y,            KC_U,            KC_I,            KC_O,            KC_P,
-    LSFT_T(KC_A),    LALT_T(KC_S),    LGUI_T(KC_D),    LCTL_T(KC_F),    KC_G,              KC_H,            RCTL_T(KC_J),    LGUI_T(KC_K),    LALT_T(KC_L),    LSFT_T(KC_QUOT),
+    LSFT_T(KC_A),    LALT_T(KC_S),    LGUI_T(KC_D),    LCTL_T(KC_F),    KC_G,              KC_H,            LCTL_T(KC_J),    LGUI_T(KC_K),    LALT_T(KC_L),    LSFT_T(KC_QUOT),
     PT_Z,            RALT_T(KC_X),    KC_C,            KC_V,            KC_B,              KC_N,            KC_M,            KC_COMM,         RALT_T(KC_DOT),  PT_SLSH,
                                       KC_LALT,         TAB_TAB,         SPC_NAV,           ENT_NUM,         BSP_BSP,         ESC_ESC
   ),
@@ -93,7 +93,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [LAYER_NUMERAL] = LAYOUT_split_3x5_3(
     XXXXXXX,         XXXXXXX,         XXXXXXX,         KC_SLASH,        KC_BSLS,           KC_MINUS,        KC_7,            KC_8,            KC_9,            KC_0,
     KC_LSFT,         KC_LALT,         KC_LGUI,         KC_LCTL,         XXXXXXX,           KC_EQUAL,        KC_4,            KC_5,            KC_6,            KC_COLN,
-    XXXXXXX,         XXXXXXX,         XXXXXXX,         XXXXXXX,         XXXXXXX,           XXXXXXX,         KC_1,            KC_2,            KC_3,            XXXXXXX,
+    KC_TILD,         XXXXXXX,         XXXXXXX,         XXXXXXX,         XXXXXXX,           XXXXXXX,         KC_1,            KC_2,            KC_3,            XXXXXXX,
                                       KC_TILD,         KC_LBRC,         KC_RBRC,           XXXXXXX,         BSP_BSP,         ESC_ESC
   ),
 
@@ -113,7 +113,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [LAYER_BASE_MAC] = LAYOUT_split_3x5_3(
     KC_Q,            KC_W,            KC_E,            KC_R,            KC_T,              KC_Y,            KC_U,            KC_I,            KC_O,            KC_P,
-    LSFT_T(KC_A),    LALT_T(KC_S),    LCTL_T(KC_D),    LGUI_T(KC_F),    KC_G,              KC_H,            RGUI_T(KC_J),    LCTL_T(KC_K),    LALT_T(KC_L),    LSFT_T(KC_QUOT),
+    LSFT_T(KC_A),    LALT_T(KC_S),    LCTL_T(KC_D),    LGUI_T(KC_F),    KC_G,              KC_H,            LGUI_T(KC_J),    LCTL_T(KC_K),    LALT_T(KC_L),    LSFT_T(KC_QUOT),
     PT_Z,            RALT_T(KC_X),    KC_C,            KC_V,            KC_B,              KC_N,            KC_M,            KC_COMM,         RALT_T(KC_DOT),  PT_SLSH,
                                       KC_LALT,         TAB_TAB,         SPC_NMC,           ENT_NMC,         BSP_BSP,         ESC_ESC
   ),
@@ -121,7 +121,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [LAYER_NUMERAL_MAC] = LAYOUT_split_3x5_3(
     XXXXXXX,         XXXXXXX,         XXXXXXX,         KC_SLASH,        KC_BSLS,           KC_MINUS,        KC_7,            KC_8,            KC_9,            KC_0,
     KC_LSFT,         KC_LALT,         KC_LCTL,         KC_LGUI,         XXXXXXX,           KC_EQUAL,        KC_4,            KC_5,            KC_6,            KC_COLN,
-    XXXXXXX,         XXXXXXX,         XXXXXXX,         XXXXXXX,         XXXXXXX,           XXXXXXX,         KC_1,            KC_2,            KC_3,            XXXXXXX,
+    KC_TILD,         XXXXXXX,         XXXXXXX,         XXXXXXX,         XXXXXXX,           XXXXXXX,         KC_1,            KC_2,            KC_3,            XXXXXXX,
                                       KC_TILD,         KC_LBRC,         KC_RBRC,           XXXXXXX,         BSP_BSP,         ESC_ESC
   ),
 
