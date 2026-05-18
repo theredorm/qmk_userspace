8df2f26b935f2ab18b4a29be9b63f222f8743b44
Andrei Strok <andrei.strok@softswiss.com>
2026-05-18 19:09:56 +0200

    fix mac

--- keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/theredorm/keymap.c
+++ keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/theredorm/keymap.c
@@ -37,8 +37,8 @@ enum dilemma_keymap_layers {
 #define DILEMMA_DEFAULT_DPI_CONFIG_STEP 1
 
 #define ESC_ESC KC_ESC
-#define MO_NAV MO(LAYER_NAVIGATION)
-#define MO_NAV_MAC MO(LAYER_NAVIGATION_MAC)
+#define MO_NUM MO(LAYER_NUMERAL)
+#define MO_NUM_MAC MO(LAYER_NUMERAL_MAC)
 
 #define TAB_TAB KC_TAB
 #define ENT_NUM LT(LAYER_NUMERAL, KC_ENT)
@@ -92,21 +92,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Q,            KC_W,            KC_E,            KC_R,            KC_T,              KC_Y,            KC_U,            KC_I,            KC_O,            KC_P,
     LSFT_T(KC_A),    LALT_T(KC_S),    LGUI_T(KC_D),    LCTL_T(KC_F),    KC_G,              KC_H,            LCTL_T(KC_J),    LGUI_T(KC_K),    LALT_T(KC_L),    LSFT_T(KC_QUOT),
     PT_Z,            RALT_T(KC_X),    KC_C,            KC_V,            KC_B,              KC_N,            KC_M,            KC_COMM,         RALT_T(KC_DOT),  PT_SLSH,
-                                      MO_NAV,         TAB_TAB,         SPC_NAV,            ENT_NUM,         BSP_BSP,         ESC_ESC
+                                      MO_NUM,         TAB_TAB,         SPC_NAV,            ENT_NUM,         BSP_BSP,         ESC_ESC
   ),
 
   [LAYER_NUMERAL] = LAYOUT_split_3x5_3(
     XXXXXXX,         XXXXXXX,         XXXXXXX,         KC_SLASH,        KC_BSLS,           KC_UNDS,        KC_7,            KC_8,            KC_9,            KC_0,
     KC_LSFT,         KC_LALT,         KC_LGUI,         KC_LCTL,         XXXXXXX,           KC_EQUAL,        KC_4,            KC_5,            KC_6,            KC_COLN,
     KC_TILD,         XXXXXXX,         XXXXXXX,         XXXXXXX,         XXXXXXX,           XXXXXXX,         KC_1,            KC_2,            KC_3,            KC_SLSH,
-                                      MO_NAV,          KC_LBRC,         KC_RBRC,           KC_ENT,         BSP_BSP,         ESC_ESC
+                                      MO_NUM,          KC_LBRC,         KC_RBRC,           KC_ENT,         BSP_BSP,         ESC_ESC
   ),
 
   [LAYER_NAVIGATION] = LAYOUT_split_3x5_3(
     XXXXXXX,         XXXXXXX,         XXXXXXX,         KC_VOLD,         KC_VOLU,           MS_WHLU,         LCTL(KC_LEFT),   XXXXXXX,         LCTL(KC_RGHT),   XXXXXXX,
     KC_LSFT,         KC_LALT,         KC_LGUI,         KC_LCTL,         MS_BTN1,           KC_LEFT,         KC_DOWN,         KC_UP,           KC_RGHT,         XXXXXXX,
     KC_TILD,         KC_RALT,         XXXXXXX,         XXXXXXX,         DRGSCRL,           MS_WHLD,         KC_HOME,         XXXXXXX,         KC_END,          KC_SLSH,
-                                      MO_NAV,         TAB_TAB,         _______,            KC_ENT,         BSP_BSP,         ESC_ESC
+                                      MO_NUM,         TAB_TAB,         _______,            KC_ENT,         BSP_BSP,         ESC_ESC
   ),
 
 
@@ -123,21 +123,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Q,            KC_W,            KC_E,            KC_R,            KC_T,              KC_Y,            KC_U,            KC_I,            KC_O,            KC_P,
     LSFT_T(KC_A),    LALT_T(KC_S),    LCTL_T(KC_D),    LGUI_T(KC_F),    KC_G,              KC_H,            LGUI_T(KC_J),    LCTL_T(KC_K),    LALT_T(KC_L),    LSFT_T(KC_QUOT),
     PT_Z,            RALT_T(KC_X),    KC_C,            KC_V,            KC_B,              KC_N,            KC_M,            KC_COMM,         RALT_T(KC_DOT),  PT_SLSH,
-                                      MO_NAV_MAC,      TAB_TAB,         SPC_NMC,        ENT_NMC,         BSP_BSP,         ESC_ESC
+                                      MO_NUM_MAC,      TAB_TAB,         SPC_NMC,           ENT_NMC,         BSP_BSP,         ESC_ESC
   ),
 
   [LAYER_NUMERAL_MAC] = LAYOUT_split_3x5_3(
-    XXXXXXX,         XXXXXXX,         XXXXXXX,         KC_SLASH,        KC_BSLS,           KC_MINUS,        KC_7,            KC_8,            KC_9,            KC_0,
+    XXXXXXX,         XXXXXXX,         XXXXXXX,         KC_SLASH,        KC_BSLS,           KC_UNDS,         KC_7,            KC_8,            KC_9,            KC_0,
     KC_LSFT,         KC_LALT,         KC_LCTL,         KC_LGUI,         XXXXXXX,           KC_EQUAL,        KC_4,            KC_5,            KC_6,            KC_COLN,
     KC_TILD,         XXXXXXX,         XXXXXXX,         XXXXXXX,         XXXXXXX,           XXXXXXX,         KC_1,            KC_2,            KC_3,            KC_SLSH,
-                                      MO_NAV_MAC,         KC_LBRC,         KC_RBRC,        KC_ENT,          BSP_BSP,         ESC_ESC
+                                      MO_NUM_MAC,         KC_LBRC,         KC_RBRC,        KC_ENT,          BSP_BSP,         ESC_ESC
   ),
 
   [LAYER_NAVIGATION_MAC] = LAYOUT_split_3x5_3(
     XXXXXXX,         XXXXXXX,         XXXXXXX,         KC_VOLD,         KC_VOLU,           MS_WHLU,         LGUI(KC_LEFT),   XXXXXXX,         LGUI(KC_RGHT),   XXXXXXX,
     KC_LSFT,         KC_LALT,         KC_LCTL,         KC_LGUI,         MS_BTN1,           KC_LEFT,         KC_DOWN,         KC_UP,           KC_RGHT,         XXXXXXX,
     KC_TILD,         KC_RALT,         XXXXXXX,         XXXXXXX,         DRGSCRL,           MS_WHLD,         KC_HOME,         XXXXXXX,         KC_END,          KC_SLSH,
-                                      MO_NAV_MAC,         TAB_TAB,         _______,        KC_ENT,         BSP_BSP,         ESC_ESC
+                                      MO_NUM_MAC,         TAB_TAB,         _______,        KC_ENT,         BSP_BSP,         ESC_ESC
   ),
 };
 // clang-format off
