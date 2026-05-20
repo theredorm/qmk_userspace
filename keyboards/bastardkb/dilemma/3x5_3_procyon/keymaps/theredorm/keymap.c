0686f2f7eca1782f89727c848ada1f98da6fa198
Andrei Strok <andrei.strok@softswiss.com>
2026-05-20 20:00:41 +0200

    test

--- keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/theredorm/keymap.c
+++ keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/theredorm/keymap.c
@@ -77,12 +77,14 @@ combo_t key_combos[] = {
 const key_override_t semicolon_colon_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_COLN, KC_SCLN);
 const key_override_t tilde_grave_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_TILD, KC_GRAVE);
 const key_override_t underscore_minus_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_UNDS, KC_MINUS);
+const key_override_t pipe_backslash_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_PIPE, KC_BSLS);
 
 // This globally defines all key overrides to be used
 const key_override_t *key_overrides[] = {
     &semicolon_colon_key_override,
     &tilde_grave_key_override,
-    &underscore_minus_key_override
+    &underscore_minus_key_override,
+    &pipe_backslash_key_override
 };
 
 // clang-format off
@@ -96,7 +98,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [LAYER_NUMERAL] = LAYOUT_split_3x5_3(
-    XXXXXXX,         XXXXXXX,         XXXXXXX,         KC_SLASH,        KC_BSLS,           KC_UNDS,        KC_7,            KC_8,            KC_9,            KC_0,
+    XXXXXXX,         XXXXXXX,         XXXXXXX,         KC_PIPE,        XXXXXXX,           KC_UNDS,        KC_7,            KC_8,            KC_9,            KC_0,
     KC_LSFT,         KC_LALT,         KC_LGUI,         KC_LCTL,         XXXXXXX,           KC_EQUAL,        KC_4,            KC_5,            KC_6,            KC_COLN,
     KC_TILD,         XXXXXXX,         XXXXXXX,         XXXXXXX,         XXXXXXX,           XXXXXXX,         KC_1,            KC_2,            KC_3,            KC_SLSH,
                                       MO_NUM,          KC_LBRC,         KC_RBRC,           KC_ENT,         BSP_BSP,         ESC_ESC
@@ -121,21 +123,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [LAYER_BASE_MAC] = LAYOUT_split_3x5_3(
     KC_Q,            KC_W,            KC_E,            KC_R,            KC_T,              KC_Y,            KC_U,            KC_I,            KC_O,            KC_P,
-    LSFT_T(KC_A),    LALT_T(KC_S),    LCTL_T(KC_D),    LGUI_T(KC_F),    KC_G,              KC_H,            LGUI_T(KC_J),    LCTL_T(KC_K),    LALT_T(KC_L),    LSFT_T(KC_QUOT),
+    LSFT_T(KC_A),    LALT_T(KC_S),    LGUI_T(KC_D),    LCTL_T(KC_F),    KC_G,              KC_H,            LCTL_T(KC_J),    LGUI_T(KC_K),    LALT_T(KC_L),    LSFT_T(KC_QUOT),
     PT_Z,            RALT_T(KC_X),    KC_C,            KC_V,            KC_B,              KC_N,            KC_M,            KC_COMM,         RALT_T(KC_DOT),  PT_SLSH,
                                       MO_NUM_MAC,      TAB_TAB,         SPC_NMC,           ENT_NMC,         BSP_BSP,         ESC_ESC
   ),
 
   [LAYER_NUMERAL_MAC] = LAYOUT_split_3x5_3(
-    XXXXXXX,         XXXXXXX,         XXXXXXX,         KC_SLASH,        KC_BSLS,           KC_UNDS,         KC_7,            KC_8,            KC_9,            KC_0,
-    KC_LSFT,         KC_LALT,         KC_LCTL,         KC_LGUI,         XXXXXXX,           KC_EQUAL,        KC_4,            KC_5,            KC_6,            KC_COLN,
+    XXXXXXX,         XXXXXXX,         XXXXXXX,         KC_PIPE,        XXXXXXX,           KC_UNDS,         KC_7,            KC_8,            KC_9,            KC_0,
+    KC_LSFT,         KC_LALT,         KC_LGUI,         KC_LCTL,         XXXXXXX,           KC_EQUAL,        KC_4,            KC_5,            KC_6,            KC_COLN,
     KC_TILD,         XXXXXXX,         XXXXXXX,         XXXXXXX,         XXXXXXX,           XXXXXXX,         KC_1,            KC_2,            KC_3,            KC_SLSH,
                                       MO_NUM_MAC,         KC_LBRC,         KC_RBRC,        KC_ENT,          BSP_BSP,         ESC_ESC
   ),
 
   [LAYER_NAVIGATION_MAC] = LAYOUT_split_3x5_3(
     XXXXXXX,         XXXXXXX,         XXXXXXX,         KC_VOLD,         KC_VOLU,           MS_WHLU,         LGUI(KC_LEFT),   XXXXXXX,         LGUI(KC_RGHT),   XXXXXXX,
-    KC_LSFT,         KC_LALT,         KC_LCTL,         KC_LGUI,         MS_BTN1,           KC_LEFT,         KC_DOWN,         KC_UP,           KC_RGHT,         XXXXXXX,
+    KC_LSFT,         KC_LALT,         KC_LGUI,         KC_LCTL,         MS_BTN1,           KC_LEFT,         KC_DOWN,         KC_UP,           KC_RGHT,         XXXXXXX,
     KC_TILD,         KC_RALT,         XXXXXXX,         XXXXXXX,         DRGSCRL,           MS_WHLD,         KC_HOME,         XXXXXXX,         KC_END,          KC_SLSH,
                                       MO_NUM_MAC,         TAB_TAB,         _______,        KC_ENT,         BSP_BSP,         ESC_ESC
   ),
