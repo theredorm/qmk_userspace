810388bd6d419d0d0f4df1d4b81b61d15562b25f
Andrei Strok <andrei.strok@softswiss.com>
2026-06-11 10:09:16 +0200

    test

--- keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/theredorm/keymap.c
+++ keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/theredorm/keymap.c
@@ -110,13 +110,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Q,            KC_W,            KC_E,            KC_R,            KC_T,              KC_Y,            KC_U,            KC_I,            KC_O,            KC_P,
     LSFT_T(KC_A),    LALT_T(KC_S),    LGUI_T(KC_D),    LCTL_T(KC_F),    KC_G,              KC_H,            LCTL_T(KC_J),    LGUI_T(KC_K),    LALT_T(KC_L),    LSFT_T(KC_QUOT),
     KC_Z,            RALT_T(KC_X),    KC_C,            KC_V,            KC_B,              KC_N,            KC_M,            KC_COMM,         KC_DOT,          KC_SLSH,
-                                      MO_NAV,          TAB_TAB,         KC_SPC,           KC_ENT_NUM,      BSP_BSP,         ESC_ESC
+                                      MO_NAV,          TAB_TAB,         KC_SPC,            KC_ENT_NUM,      BSP_BSP,         ESC_ESC
   ),
 
   [LAYER_NUMERAL] = LAYOUT_split_3x5_3(
     KC_1,            KC_2,            KC_3,            KC_4,            KC_5,              KC_6,            KC_7,            KC_8,            KC_9,            KC_0,
     KC_LSFT,         KC_LALT,         KC_LGUI,         KC_LCTL,         XXXXXXX,           KC_MINUS,        XXXXXXX,         XXXXXXX,         KC_LALT,         SFT_COLN,
-    KC_TILD,         KC_PIPE,         XXXXXXX,         XXXXXXX,         XXXXXXX,           KC_EQUAL,        XXXXXXX,         KC_COMM,         KC_DOT,          PT_SLSH,
+    PT_TILD,         KC_PIPE,         XXXXXXX,         XXXXXXX,         XXXXXXX,           KC_EQUAL,        XXXXXXX,         KC_COMM,         KC_DOT,          PT_SLSH,
                                       MO_NAV,          KC_LBRC,         KC_RBRC,           KC_ENT_NUM,      BSP_BSP,         ESC_ESC
   ),
 
@@ -146,7 +146,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [LAYER_NUMERAL_MAC] = LAYOUT_split_3x5_3(
     KC_1,            KC_2,            KC_3,            KC_4,            KC_5,              KC_6,            KC_7,            KC_8,            KC_9,            KC_0,
     KC_LSFT,         KC_LALT,         KC_LGUI,         KC_LCTL,         XXXXXXX,           KC_MINUS,        XXXXXXX,         XXXXXXX,         KC_LALT,         SFT_COLN,
-    KC_TILD,         KC_PIPE,         XXXXXXX,         XXXXXXX,         XXXXXXX,           KC_EQUAL,        XXXXXXX,         XXXXXXX,         XXXXXXX,         PT_SLSH,
+    PT_TILD,         KC_PIPE,         XXXXXXX,         XXXXXXX,         XXXXXXX,           KC_EQUAL,        XXXXXXX,         KC_COMM,         KC_DOT,          PT_SLSH,
                                       MO_NAV_MAC ,     KC_LBRC,         KC_RBRC,           KC_ENT_NUM_MAC,  BSP_BSP,         ESC_ESC
   ),
 
